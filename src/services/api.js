af66a63bd61113c3c018ca96319fc3b079aecce2
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2020-07-26 19:10:39 -0300

    att api.js || att yarn version

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-require('dotenv').config()
 import axios from 'axios'; // axios vai nos permitir conversar com o backend
+require('dotenv').config()
 
 // const api = axios.create({baseURL: 'http://localhost:3001/api'}); // aqui fica a conexão com o backend, e deve ficar o link para ele
 const api = axios.create({baseURL: process.env.REACT_APP_API_URL /* https://coverme-backend.herokuapp.com/api */}); // aqui fica a conexão com o backend, e deve ficar o link para ele
