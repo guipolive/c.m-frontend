4f46caaa1be6e37c33fe5f907143fbd43222b013
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2020-07-26 18:21:51 -0300

    add dotenv lib

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 import axios from 'axios'; // axios vai nos permitir conversar com o backend
 
 // const api = axios.create({baseURL: 'http://localhost:3001/api'}); // aqui fica a conexão com o backend, e deve ficar o link para ele
-const api = axios.create({baseURL: 'https://coverme-backend.herokuapp.com/api'}); // aqui fica a conexão com o backend, e deve ficar o link para ele
+const api = axios.create({baseURL: process.env.REACT_APP_API_URL /* https://coverme-backend.herokuapp.com/api */}); // aqui fica a conexão com o backend, e deve ficar o link para ele
 
 export default api; // usarmemos 'api' sempre que formos fazer conexão com a api
