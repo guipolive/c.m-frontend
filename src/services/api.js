f53ed0abdea421af1368aa700569f4aef9dc6081
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2020-08-12 16:35:33 -0300

    att backend connection

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios'; // axios vai nos permitir conversar com o backend
 require('dotenv').config()
 
-// const api = axios.create({baseURL: 'http://localhost:3001/api'}); // aqui fica a conexão com o backend, e deve ficar o link para ele
-const api = axios.create({baseURL: process.env.REACT_APP_API_URL /* https://coverme-backend.herokuapp.com/api */}); // aqui fica a conexão com o backend, e deve ficar o link para ele
+/*
+    A variável REACT_APP_API_URL será igual a:
+    localhost usado pelo backend
+    ou
+    https://coverme-backend.herokuapp.com/api
+*/
+const api = axios.create({baseURL: process.env.REACT_APP_API_URL }); // aqui fica a conexão com o backend, e deve ficar o link para ele
 
 export default api; // usarmemos 'api' sempre que formos fazer conexão com a api
